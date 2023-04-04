948b7f8c69ab894878a028703a191ed015dac2f3
Victor Hugo Sánchez <hugo71119@gmail.com>
2023-04-04 15:16:40 -0600

    first commit

---
+++ pages/nosotros.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Nosotros() {
+  return (
+    <div>
+      <h1>Desde Nosotros</h1>
+
+      <Link href="/">Ir a Inicio</Link>
+      <Link href="/servicios">Ir a Servicios</Link>
+    </div>
+  )
+}
---
+++ pages/servicios.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default function Servicios() {
+    return (
+        <div>
+            <h1>
+                Desde Servicios
+        </h1>
+
+
+            <Link href="/">Ir a Inicio</Link>
+            <Link href="/nosotros">Ir a Nosotros</Link>
+        </div>
+    )
+}
